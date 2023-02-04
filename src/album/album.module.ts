827b8e3374a5a85579a06ca7017d4cd793448cf4
Maksim, Rafalko <maks.rafalko@gmail.com>
2023-02-05 01:37:01 +0300

    refactor: when album is removed, remove links from tracks to this albums

--- src/album/album.module.ts
+++ src/album/album.module.ts
@@ -2,9 +2,11 @@ import { Module } from '@nestjs/common';
 import { AlbumService } from './album.service';
 import { AlbumController } from './album.controller';
 import { AlbumRepository } from '../database/album.repository';
+import { TrackModule } from '../track/track.module';
 
 @Module({
   controllers: [AlbumController],
   providers: [AlbumService, AlbumRepository],
+  imports: [TrackModule],
 })
 export class AlbumModule {}
--- src/album/album.service.ts
+++ src/album/album.service.ts
@@ -3,10 +3,14 @@ import { CreateAlbumDto } from './dto/create-album.dto';
 import { UpdateAlbumDto } from './dto/update-album.dto';
 import { Album } from './entities/album.entity';
 import { AlbumRepository } from '../database/album.repository';
+import { TrackRepository } from '../database/track.repository';
 
 @Injectable()
 export class AlbumService {
-  constructor(private albumRepository: AlbumRepository) {}
+  constructor(
+    private albumRepository: AlbumRepository,
+    private trackRepository: TrackRepository,
+  ) {}
 
   async create(createAlbumDto: CreateAlbumDto) {
     return await this.albumRepository.create(createAlbumDto);
@@ -34,5 +38,13 @@ export class AlbumService {
 
   async remove(uuid: string): Promise<void> {
     await this.albumRepository.remove(uuid);
+
+    // remove "foreign keys" - links to tracks
+    const tacks = await this.trackRepository.findByAlbumId(uuid);
+
+    for (const track of tacks) {
+      track.albumId = null;
+      await this.trackRepository.update(track.id, track);
+    }
   }
 }
